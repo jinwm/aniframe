ff34b4956e9a4ff750c5b152391968153d7709d8
JINWM <99179341+jinwm@users.noreply.github.com>
2024-01-08 17:43:26 +0800

    edit

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 (function (window, document) {
-    var container = null, 
-        imgs = [], 
-        width = null, 
+    // 变量声明
+    var container = null,
+        imgs = [],
+        width = null,
         height = null,
         speed = 100,
         loop = false,
         autoplay = false,
-        autoLoadImgs = true, 
+        autoLoadImgs = true,
         coverSrc = null,
         onReady = null,
         onPlaying = null,
-        onPlay = null, 
-        onEnded = null, 
-        progress = 0, 
-        curIndex = 0, 
-        isInit = false, 
+        onPlay = null,
+        onEnded = null,
+        progress = 0,
+        curIndex = 0,
+        isInit = false,
         lastTime = Date.now(),
         loader = [],
-        isPlay = false, 
+        isPlay = false,
         cvs = document.createElement('canvas'),
         ctx = cvs.getContext('2d');
 
+    // requestAnimationFrame的兼容性处理
     window.requestAnimationFrame = window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame ||
         function (callback) {
             window.setTimeout(callback, speed);
         }
+
+    // 类定义
     class AniFrame {
         constructor(options) {
+            // 将选项或默认值分配给变量
             container = options.container ? options.container : container;
             imgs = options.imgs ? options.imgs : imgs;
             width = options.width ? options.width : options.container.clientWidth;
@@ -43,6 +48,7 @@
             onPlay = typeof options.onPlay === 'function' ? options.onPlay : null;
             onEnded = typeof options.onEnded === 'function' ? options.onEnded : null;
 
+            // 设置画布属性
             cvs.style.width = '100%';
             cvs.style.height = 'auto';
             cvs.style.inset = '0';
@@ -52,15 +58,18 @@
             cvs.width = width;
             cvs.height = height;
 
+            // 如果autoLoadImgs为true，则加载图片
             if (autoLoadImgs) loadImages();
         }
     }
 
+    // Load方法触发图片加载
     AniFrame.prototype.load = function () {
-        if(isInit) return;
+        if (isInit) return;
         if (!autoLoadImgs) loadImages();
     }
 
+    // 播放方法
     AniFrame.prototype.play = function () {
         if (isPlay) return;
         isPlay = true;
@@ -70,10 +79,12 @@
         });
     }
 
+    // 暂停方法
     AniFrame.prototype.pause = function () {
         isPlay = autoplay = false;
     }
 
+    // 销毁方法
     AniFrame.prototype.destroy = function () {
         this.pause();
 
@@ -97,6 +108,7 @@
         curIndex = 0;
     };
 
+    // 加载图片的方法
     function loadImages(callback) {
         while (container.firstChild) {
             container.removeChild(container.firstChild);
@@ -123,6 +135,7 @@
         }
     }
 
+    // 设置封面的方法
     function setCover() {
         var coverImg = new Image();
         coverImg.src = coverSrc;
@@ -132,15 +145,17 @@
         }
     }
 
+    // 绘制动画的方法
     function draw(callback) {
         callback && callback();
         onPlay && onPlay();
         animation();
     }
 
+    // 动画循环
     function animation() {
         window.requestAnimationFrame(animation);
-        var nowTime = Date.now(); 
+        var nowTime = Date.now();
         if (isPlay && (nowTime - lastTime) >= speed) {
             if (curIndex >= loader.length) {
                 if (loop) {
@@ -159,5 +174,6 @@
         }
     }
 
+    // 将AniFrame暴露给全局对象
     window.AniFrame = AniFrame;
 })(window, document);
